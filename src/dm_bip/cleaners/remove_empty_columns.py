751f6bafc0adddca948ed5334d44185058d011fe
Patrick Golden <ptgolden@email.unc.edu>
2025-05-19 12:06:47 -0400

    Linting on remove_empty_columns.py
    Errors weren't being flagged earlier because it didn't live in src/

--- src/dm_bip/cleaners/remove_empty_columns.py
+++ src/dm_bip/cleaners/remove_empty_columns.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
 
-import pandas as pd
 import argparse
-import sys
 import signal
+import sys
+
+import pandas as pd
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Clean a TSV file by dropping columns where all values are NaN.")
-    parser.add_argument("input_file", nargs="?", type=argparse.FileType('r'), default=sys.stdin, help="Path to the input TSV file or stdin")
-    parser.add_argument("-o", "--output", type=argparse.FileType('w'), default=sys.stdout, help="Path to the output TSV file or stdout")
+    parser.add_argument(
+        "input_file",
+        nargs="?",
+        type=argparse.FileType("r"),
+        default=sys.stdin,
+        help="Path to the input TSV file or stdin",
+    )
+    parser.add_argument(
+        "-o", "--output", type=argparse.FileType("w"), default=sys.stdout, help="Path to the output TSV file or stdout"
+    )
     args = parser.parse_args()
 
     return args
+
 
 def remove_empty_columns(df, output):
     df = pd.read_csv(df, sep="\t")  # Read TSV
@@ -22,9 +33,11 @@ def remove_empty_columns(df, output):
     except BrokenPipeError:
         sys.stderr.close()
 
+
 def main():
     args = parse_args()
     remove_empty_columns(args.input_file, args.output)
+
 
 if __name__ == "__main__":
     # Handle SIGPIPE to prevent broken pipe errors
