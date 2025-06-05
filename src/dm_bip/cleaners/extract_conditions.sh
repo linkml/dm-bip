a01a203ba1f60156556b460ddfd4679df2684b1c
Trish Whetzel <plwhetzel@gmail.com>
2025-06-05 01:23:21 -0700

    extract only unique conditions

--- src/dm_bip/cleaners/extract_conditions.sh
+++ src/dm_bip/cleaners/extract_conditions.sh
@@ -12,12 +12,14 @@ base_name=$(basename "$input_file" .tsv)
 # Construct the full output path in the same directory
 output_file="${input_dir}/${base_name}-conditions.tsv"
 
-# Add column header and extract headers from the specified starting column to the end
+# Extract headers from the specified starting column to the end,
+# filter for case-insensitive unique values, and add column header
 {
-  echo -e "conditions"
-  head -n 1 "$input_file" | awk -v start="$start_col" -F'\t' '{
-    for (i = start; i <= NF; i++) print $i
-  }'
+  echo "conditions"
+  head -n 1 "$input_file" | \
+    awk -v start="$start_col" -F'\t' '{
+      for (i = start; i <= NF; i++) print $i
+    }' | awk '{ lc[tolower($0)] = $0 } END { for (k in lc) print lc[k] }' | sort
 } > "$output_file"
 
 echo "Wrote condition headers to: $output_file"
